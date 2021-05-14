07732e5e30bfe25c60214fee071ab9b5f7d84c48
Adrian Kaczmarek <k_adrian@wp.pl>
2021-05-14 11:36:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('intro__nav-list');
+
+
+hamburger.addEventListener('click', ()=>{
+
+        navUL.classList.toggle('show');
+
+
+});
